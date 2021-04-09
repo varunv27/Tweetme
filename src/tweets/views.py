79e55f160209f00bf355168aba1d13203c62d0ce
varunv27 <varun.att@gmail.com>
2021-04-09 20:18:08 +0000

    Added Bootstrap layout to Homepage

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -11,7 +11,7 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     '''
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,2000000)} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,200)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
